357eba463a75218077bda50c036ee72d78ab7f23
Leonardo Cortes <lionelspagna@gmail.com>
2024-09-17 19:30:51 +0200

    Updated

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -27,6 +27,7 @@ regd_users.post("/login", (req,res) => {
       const token = jwt.sign({ username: username }, JWT_SECRET);
       req.session.username = username;
       req.session.token = token;
+      req.session.authorization = { username: username, accessToken: token };
       message = "Successfully logged, token: " + req.session.token;
     } else {
       message = "Password not valid";
@@ -40,8 +41,68 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
+  const username = req.session.username;
+  const review = req.body.review;
+  if(!review) {
+    return res.status(400).send("Review field is missing");
+  }
+  const isbn = req.params.isbn;
+  let filtered_book = {};
+  for(let bookId in books) {
+    let book = books[bookId];
+    //console.log(book.author);
+    if(book.isbn?.includes(isbn)) filtered_book = book;
+  }
+  let counter = 0;
+  //if filtered_book founded
+  if(filtered_book) {
+    let filtered_review = null;
+    
+    for(let id_review in filtered_book.reviews) {
+      console.log('id_review', id_review);
+      let currentReview = filtered_book.reviews[id_review];
+      console.log('current review', currentReview);
+      if(currentReview.username == username) {
+        currentReview.review = { username: username, review: review };
+        filtered_review = currentReview;
+        filtered_book.reviews[id_review] = currentReview.review;
+      }
+      counter++;
+    }
+    if(!filtered_review) {
+      console.log('Adding')
+      filtered_book.reviews = { ...filtered_book.reviews,  [counter.toString()] : { username: username, review: review } };
+    }
+  }
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(filtered_book);
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const username = req.session.username;
+  const isbn = req.params.isbn;
+  let filtered_book = {};
+
+  for(let bookId in books) {
+    let book = books[bookId];
+    //console.log(book.author);
+    if(book.isbn?.includes(isbn)) filtered_book = book;
+  }
+  
+  //if filtered_book founded
+  if(filtered_book) {
+    
+    for(let id_review in filtered_book.reviews) {
+      let currentReview = filtered_book.reviews[id_review];
+      if(currentReview.username == username) {
+        delete filtered_book.reviews[id_review];
+        return res.send("Content deleted");
+      }
+    }
+  } else {
+    return res.status(404).send("Not found");
+  }
+  return res.status(404).send("Not found");
 });
 
 module.exports.authenticated = regd_users;
